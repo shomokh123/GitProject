a9a620d277cdfd4ef2e1ff1ad8e25889ebdaa990
shomokh123 <ip5115@outlook.sa>
2023-10-05 01:52:29 -0700

    Initial Commit

---
+++ GitProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitProject
+//
+//  Created by shomokh aldosari on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by shomokh aldosari on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
